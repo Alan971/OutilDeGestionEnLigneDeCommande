b7b656500973300cd4e07dde2bf7213a78e3f0af
ALAN <alanrouxel@hotmail.com>
2024-06-13 12:48:46 -0400

    factorisation de la classe ContactManager.php

--- class/ContactManager.php
+++ class/ContactManager.php
@@ -7,34 +7,36 @@ use RuntimeException;
 
 class ContactManager{
 
+    private $db;
+    
+    public function __construct()
+    {
+        $this->db = (new DBConnect())->getPDO();
+    }
+
     public function findAll() : array
     {   
-        $dbConnect = new DBConnect();
-        $db = $dbConnect->getPDO();
-        $dbQuerry = $db->prepare("SELECT * FROM contact");
+        
+        $dbQuerry = $this->db->prepare("SELECT * FROM contact");
         $dbQuerry->execute();
         return $dbQuerry->fetchAll();
     }
     public function createContact($newContact) :void
     {
-        $dbConnect = new DBConnect();
-        $db = $dbConnect->getPDO();
-        $dbQuerry = $db->prepare("INSERT INTO contact (`id`, `name`, `email`, `phone_number`) VALUES (NULL,?,?,?)");
+        $dbQuerry = $this->db->prepare("INSERT INTO contact (`id`, `name`, `email`, `phone_number`) VALUES (NULL,?,?,?)");
         $dbQuerry->execute([$newContact[1],$newContact[2],$newContact[3]]);
         $dbQuerry->fetchAll();
     }
     public function deleteContact($id) : void
     {
-        $db = (new DBConnect())->getPDO();
-        $dbQuerry = $db->prepare("DELETE FROM contact WHERE 'id'=:id");
+        $dbQuerry = $this->db->prepare("DELETE FROM contact WHERE 'id'=:id");
         $dbQuerry->execute(['id'=>$id]);
         $dbQuerry->fetchAll();
     }
 
     public function modifyContact($Contact) :void
     {
-        $db = (new DBConnect())->getPDO();
-        $dbQuerry = $db->prepare("UPDATE contact SET  `name`= :name, `email`= :email , `phone_number`= :phone_number WHERE id= :id");
+        $dbQuerry = $this->db->prepare("UPDATE contact SET  `name`= :name, `email`= :email , `phone_number`= :phone_number WHERE id= :id");
         $dbQuerry->execute(['name'=>$Contact['name'], 'email'=>$Contact['email'],'phone_number'=>$Contact['phone_number'],'id'=>$Contact['id']]);
         $dbQuerry->fetchAll();
     }
